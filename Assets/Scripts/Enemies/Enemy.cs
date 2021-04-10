25e285aee232de14e026f7e9c925e69c0b6fa172
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2021-04-09 21:10:51 -0400

    Enemy Completed with the ability to shoot

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    public GameObject projectile;
+    private float timeBtweenShots;
+    public float startTimeBtweenshots;
+    public Transform BulletSpawn;
+
+    private Transform player;
+    private float positionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        Debug.Log(player.name);
+        positionY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+
+        if(Vector3.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, positionY, transform.position.z), player.position, speed * Time.deltaTime);
+        }
+        else if(Vector3.Distance(transform.position, player.position) < stoppingDistance 
+            && Vector3.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if(Vector3.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, positionY, transform.position.z), player.position, -speed * Time.deltaTime);
+        }
+
+
+        if(timeBtweenShots <= 0)
+        {
+            Instantiate(projectile, new Vector3(BulletSpawn.position.x, BulletSpawn.position.y, BulletSpawn.position.z),transform.rotation * Quaternion.Euler(90,45,0));
+            timeBtweenShots = startTimeBtweenshots;
+        }
+        else
+        {
+            timeBtweenShots -= Time.deltaTime;
+        }
+
+    }
+}
--- Assets/Scripts/Weapons/AK47Bullet.cs
+++ Assets/Scripts/Weapons/AK47Bullet.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class AK47Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed;
+    public float timeRemaining = 5;
+
+    private Transform player;
+    private Vector3 target;
+    float distance;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector3(player.position.x, player.position.y, player.position.z);
+        transform.LookAt(player);
+        transform.rotation *= Quaternion.Euler(90, 0, 0);
+        distance = Vector3.Distance(target, transform.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.x, target.y + 1, target.z), speed * Time.deltaTime);
         
+        if(timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            DestroyProjectile();
+        }
+        Debug.Log(distance);
+        if (distance < 3)
+        {
+            DestroyProjectile();
+        }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Ow");
+            DestroyProjectile();
+        }
+    }
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
 }
